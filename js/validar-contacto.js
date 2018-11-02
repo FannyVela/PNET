1dde40e42e4a4a288adcc3a036bde2ee80e41145
fannyvd97 <33985674+fannyvd97@users.noreply.github.com>
2018-11-03 00:27:40 +0100

    validacion contacto + footer

---
+++ js/validar-contacto.js
@@ -0,0 +1,50 @@
+function validar()
+{
+	var formulario= document.getElementsByName("formulario")[0];
+	
+	var nombre = formulario.name.value;
+	var apellido1 = formulario.surname1.value;
+	var apellido2 = formulario.surname2.value;
+	var correo = formulario.email.value;
+	var tlf = formulario.phone.value;
+
+	var mensaje
+	var expreg = /^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/;
+	
+	var expregCorreo = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/ ;
+	
+	var expregTlf = /^[9|6]{1}([\d]{2}[-]*){3}[\d]{2}$/;
+	
+	if(!expreg.test(nombre))
+	{
+			alert("Nombre inválido.");
+			return false;
+	} 
+	
+	if(!expreg.test(apellido1))
+	{
+			alert("1º Apellido inválido.");
+			return false;
+	} 
+
+	if(!expreg.test(apellido2) && apellido2 != "")
+	{
+		alert("2º Apellido inválido.");
+		return false;
+	}
+	
+	if(!expregCorreo.test(correo))
+	{
+		alert("Correo inválido");
+		return false;
+	}
+	
+	if(!expregTlf.test(tlf))
+	{
+		alert("Teléfono inválido.");
+		return false;
+	}
+
+	return true;
+		
+}
