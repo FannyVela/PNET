e4dd7aa3c9eee9f41054c874e716ef3e73ebf439
fannyvd97 <33985674+fannyvd97@users.noreply.github.com>
2018-11-27 18:06:57 +0100

    arreglos

--- apiCongresoCliente/js/wsinvocations.js
+++ apiCongresoCliente/js/wsinvocations.js
@@ -1,4 +1,4 @@
-function postInscripcion(nom, a1, a2, dni, mail, tel, n, d1, d2, p) {
+function postInscripcion(nom, s1, s2, dni, email, tel, days, prensa) {
     $.ajax({
         type: "POST",
         url: "http://localhost:8080/attendees/",
@@ -6,18 +6,16 @@ function postInscripcion(nom, a1, a2, dni, mail, tel, n, d1, d2, p) {
         dataType: "text",
         data: JSON.stringify({
             "name": nom,
-            "name1": a1,
-            "name2": a2,
-			"dni": dni,
-			"mail": mail,
-			"phone": tel,
-			"days": n,
-			"day1": d1,
-			"day2": d2,
-			"press": p
+            "surname1": s1,
+            "surname2": s2,
+			      "dni": dni,
+      			"email": email,
+      			"phone": tel,
+      			"days": days,
+      			"prensa": prensa
         }),
         success: function(data) {
-            $("#resGetHello").html(data);
+            $("#resul").html(data);
         },
         error: function(res) {
             alert("ERROR " + res.statusText);
@@ -30,7 +28,25 @@ function getInscripciones(){
       type: "GET",
       url: "http://localhost:8080/attendees/",
       success: function(data){
-              $("#resGetHello").html(JSON.stringify(data));      },
+        var attendees = data;
+        var HTMLText  = "<ul>";
+
+        for( var i = 0 ; i < attendees.length ; i ++){
+          var attendeeAux = attendees[i];
+          HTMLText += "<li> Nombre: " + attendeeAux.name +
+                            "<br>Apellido 1: " + attendeeAux.surname1 +
+                            "<br>Apellido 2: " + attendeeAux.surname2 +
+                            "<br>DNI: " + attendeeAux.dni +
+                            "<br>Email: " + attendeeAux.email +
+                            "<br>Teléfono: " + attendeeAux.phone +
+                            "<br>Días de asistencia: " + attendeeAux.days +
+                            "<br>Prensa: " + attendeeAux.prensa +
+                      "</li><br><br>";
+        }
+        HTMLText +="</ul>";
+        $("#resul").html(HTMLText);
+      /*      $("#resul").html(JSON.stringify(data));  */
+     },
       error:function(res){
               alert("ERROR: "+ res.statusText);  }
   });
@@ -41,22 +57,40 @@ function getInscripcion(id){
       type: "GET",
       url: "http://localhost:8080/attendees/" + id,
       success: function(data){
-              $("#resGetHello").html(JSON.stringify(data));      },
+          var attendee = data;
+          var HTMLText =    "<p>Nombre: " + attendee[0].name +
+                            "<br>Apellido 1: " + attendee[0].surname1 +
+                            "<br>Apellido 2: " + attendee[0].surname2 +
+                            "<br>DNI: " + attendee[0].dni +
+                            "<br>Email: " + attendee[0].email +
+                            "<br>Teléfono: " + attendee[0].phone +
+                            "<br>Días de asistencia: " + attendee[0].days +
+                            "<br>Prensa: " + attendee[0].prensa; + "</p>"
+            $("#resul").html(HTMLText);
+      },
       error:function(res){
               alert("ERROR: "+ res.statusText);  }
   });
 }
 
 //function putInscripcion(id, nom, a1, a2, dni, mail, tel, n, d1, d2, p){
-function putInscripcion(id, nom){
+function putInscripcion(id, nom, surname1, surname2, dni, email, phone, dias, prensa){
  $.ajax({
       type: "PUT",
       url: "http://localhost:8080/attendees/" + id,
       data:{
-            "name": nom
+            "name": nom,
+            "surname1": surname1,
+            "surname2": surname2,
+            "dni": dni,
+            "email": email,
+            "phone": phone,
+            "days": dias,
+            "prensa": prensa
         },
       success: function(data){
-              $("#resGetHello").html(JSON.stringify(data));      },
+            var HTMLText = "Asistente actualizado con éxito.";
+              $("#resul").html(HTMLText);      },
       error:function(res){
               alert("ERROR: "+ res.statusText);  }
   });
@@ -67,7 +101,9 @@ function deleteInscripcion(id){
       type: "DELETE",
       url: "http://localhost:8080/attendees/" + id,
       success: function(data){
-              $("#resGetHello").html(JSON.stringify(data));      },
+          var HTMLText = "Asistente eliminado con éxito";
+              $("#resul").html(HTMLText);
+      },
       error:function(res){
               alert("ERROR: "+ res.statusText);  }
   });
@@ -78,7 +114,8 @@ function deleteInscripciones(){
       type: "DELETE",
       url: "http://localhost:8080/attendees/",
       success: function(data){
-              $("#resGetHello").html(JSON.stringify(data));      },
+              var HTMLText = "Los asistentes fueron eliminados con éxito.";
+              $("#resul").html(HTMLText);      },
       error:function(res){
               alert("ERROR: "+ res.statusText);  }
   });
